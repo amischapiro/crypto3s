711126036d718c91c21ea59a5ac1a69ae6adeb11
roy schwartz <royschwartz9@gmail.com>
2023-06-08 17:13:09 +0300

    roystam

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000; 
-
+/////
 const products = [
     { id:1,name: 'Product 1', description: 'Description 1', price: 10 },
     { id:2,name: 'Product 2', description: 'Description 2', price: 20 },
