3eb9a28a1639e170538031aaf180d1899e70c300
amischapiro <amischapiro@gmail.com>
2023-06-09 08:44:14 +0300

    addProduct,DB connection

--- server.js
+++ server.js
@@ -1,32 +1,22 @@
 const express = require('express');
 const app = express();
-// const {default:mongoose}= require("mongoose")
-// const Product = require('.models/products')
-// mongoose.connect('mongodb://0.0.0.0:27017/homedb',{useNewUriParser:true})
-// .then(()=>{
-//   console.log("mongo is connected");
-// }).catch(err=>{
-//   console.log("no connection");
-// })
-//start connection with mongoDB
 const port = 3000; 
 
-const products = [
-    { id:1,name: 'Product 1', description: 'Description 1', price: 10 },
-    { id:2,name: 'Product 2', description: 'Description 2', price: 20 },
-    { id:3,name: 'Product 3', description: 'Description 3', price: 30 },
-    { id:4,name: 'Product 4', description: 'Description 4', price: 40 },
-    { id:5,name: 'Product 5', description: 'Description 5', price: 50 },
-];
+const {default:mongoose}= require("mongoose")
+const Product = require('./models/product')
+mongoose.connect('mongodb+srv://royschwartz:Aa123456@crypto.tcprfjj.mongodb.net/crypto?retryWrites=true&w=majority')
+.then(()=>{
+  console.log("mongo is connected");
+}).catch(err=>{
+  console.log("no connection",err);
+})
+
 
 app.set('view engine', 'ejs');
 
-// app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 
-// add async before (req,res)
-//newproduct.save() saves it to our DB
 app.get('/', (req, res) => {
     res.render('home', { title: 'Home' });
   });
@@ -35,13 +25,43 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.render('products', { title: 'Products Page',products });
+  Product.find()
+  .then(products => {
+    res.render('products', { title: 'Products Page',products });
+  })
+  .catch(error => {
+    console.error('Error fetching products:', error);
+  });
+});
+app.get('/addProduct', (req, res) => {
+  Product.find()
+  .then(products => {
+    res.render('addProduct', { title: 'addProducts Page',products });
+  })
+  .catch(error => {
+    console.error('Error fetching products:', error);
+  });
 });
 app.get('/product-info/:productId', (req, res) => {
     const productId = req.params.productId;
-    const product = products.find((p) => p.id == productId);
-  res.render('product-info', { title: 'Product Info Page',product});
+    Product.findOne({ _id: productId })
+    .then(product => {
+      if (product) {
+        res.render('product-info', { title: 'Product Info Page',product});
+      } else {
+        console.log('Product not found');
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching product:', error);
+    });
+  
+
 });
+app.post('/addProduct',(req,res)=>{
+  console.log(req.body);
+  // gets info from addProduct form
+})
 
 app.get('/cart', (req, res) => {
   res.render('cart', { title: 'Cart Page' });
